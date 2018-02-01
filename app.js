ea6a581d00dc24619313f15a9cd2496113ef19c7
nascal3 <nascal3@gmail.com>
2018-02-01 23:53:27 +0300

    add color change according to expense or income

--- app.js
+++ app.js
@@ -152,6 +152,12 @@ var UIController = (function() {
         dateLabel: '.budget__title--month'
     }
 
+    var nodeListForEach = function(list, callback) {
+        for (var i = 0; i < list.length; i++) {
+            callback(list[i], i);
+        }
+    };
+
     return {
       getInput: function() {
           return {
@@ -215,13 +221,7 @@ var UIController = (function() {
         displayPercentages: function(percentages) {
            var fields =  document.querySelectorAll(DOMstrings.expensesPercLabel);
 
-           var nodeListForEach = function(list, callback) {
-               for (var i = 0; i < list.length; i++) {
-                   callback(list[i], i);
-               }
-           };
-
-            nodeListForEach(fields, function (current, index) {
+           nodeListForEach(fields, function (current, index) {
                 if(percentages[index] > 0) {
                     current.textContent = percentages[index] + '%';
                 } else {
@@ -240,7 +240,20 @@ var UIController = (function() {
           month = now.getMonth();
           year = now.getFullYear();
           document.querySelector(DOMstrings.dateLabel).textContent = months[month] + ' ' + year;
+        },
 
+        changedType: function() {
+
+          var fields = document.querySelectorAll(
+              DOMstrings.inputType + ',' +
+              DOMstrings.inputDesc + ',' +
+              DOMstrings.inputValue
+          );
+
+          nodeListForEach(fields, function(cur) {
+              cur.classList.toggle('red-focus');
+          });
+          document.querySelector(DOMstrings.inputBtn).classList.toggle('red');
 
         },
 
@@ -266,6 +279,8 @@ var controller = (function(budgetCtrl, UICtrl) {
         });
 
         document.querySelector(DOM.container).addEventListener('click', ctrlDeleteItem);
+
+        document.querySelector(DOM.inputType).addEventListener('change', UICtrl.changedType);
     };
 
     var updateBudget = function() {
